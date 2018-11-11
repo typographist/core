272fef0585174bbf9a12f39d993346db6baac297
maxinakenty <mgreen097@gmail.com>
2018-11-11 17:32:21 +0300

    rename make-breakpoints-model

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 module.exports = {
   ...require('./src/api/breakpoints'),
-  ...require('./src/api/makeBreakpointsModel'),
+  ...require('./src/api/make-breakpoints-model'),
   ...require('./src/api/modularScale'),
   ...require('./src/api/userConfigValidator'),
-}
+};
