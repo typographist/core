f5ff2dce9a6eb6fba5c8fda2bdc593389fe9c523
maxinakenty <mgreen097@gmail.com>
2018-11-18 05:54:19 +0300

    camelize refactoring

--- src/helpers/camelize/index.js
+++ src/helpers/camelize/index.js
@@ -1,16 +1,24 @@
+import R from 'ramda';
 import { DASH_HYPHEN_WHITESPACE_ANY_CHARACTERS } from '../../constants/regexes';
-import { isNumeric } from '../is-numeric';
+
+const camelizeString = R.replace(
+  DASH_HYPHEN_WHITESPACE_ANY_CHARACTERS,
+  (match, chr) => (chr ? chr.toUpperCase() : ''),
+);
+
+const camelizeStringProcess = R.compose(
+  R.reduce((acc, item) => acc + item, ''),
+  R.juxt([
+    R.compose(
+      R.toLower,
+      R.head,
+    ),
+    R.compose(
+      camelizeString,
+      R.tail,
+    ),
+  ]),
+);
 
 // camelize :: String => String
-export const camelize = value => {
-  let string = value;
-
-  if (isNumeric(string)) {
-    return string;
-  }
-  string = string.replace(DASH_HYPHEN_WHITESPACE_ANY_CHARACTERS, (match, chr) =>
-    chr ? chr.toUpperCase() : '',
-  );
-  // Ensure 1st char is always lowercase
-  return string.substr(0, 1).toLowerCase() + string.substr(1);
-};
+export const camelize = camelizeStringProcess;
