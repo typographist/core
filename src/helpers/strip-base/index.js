021b5564659e2d1afbf1771491f36d95d492e547
maxinakenty <mgreen097@gmail.com>
2018-11-18 05:53:55 +0300

    strip-base refactoring

--- src/helpers/strip-base/index.js
+++ src/helpers/strip-base/index.js
@@ -1,22 +1,22 @@
-import {
-  INTEGER_OR_FLOATING_POINT_NUMBER_WITH_PX_UNIT,
-  INTEGER_OR_FLOATING_POINT_NUMBER_WITH_EM_UNIT,
-} from '../../constants/regexes';
+import R from 'ramda';
+import { INTEGER_OR_FLOATING_POINT_NUMBER_WITH_EM_UNIT } from '../../constants/regexes';
 import { toPx } from '../to-px';
 
-//  stripBase :: (Array[String] | String) => Array[Number] | Number
-export const stripBase = x => {
-  let result = null;
+//  hasEms :: String -> Boolean
+const hasEms = R.test(INTEGER_OR_FLOATING_POINT_NUMBER_WITH_EM_UNIT);
 
-  if (Array.isArray(x)) {
-    result = x.map(item => stripBase(item));
-  } else if (typeof x === 'string') {
-    if (INTEGER_OR_FLOATING_POINT_NUMBER_WITH_PX_UNIT.test(x)) {
-      result = parseFloat(x);
-    }
-    if (INTEGER_OR_FLOATING_POINT_NUMBER_WITH_EM_UNIT.test(x)) {
-      result = parseFloat(toPx(x));
-    }
-  }
-  return result;
-};
+// baseToPx :: String -> String
+const baseToPx = R.ifElse(
+  hasEms,
+  R.compose(
+    parseFloat,
+    toPx,
+  ),
+  parseFloat,
+);
+
+// stripBase :: (Array[String] | String) => Array[Number] | Number
+export const stripBase = R.cond([
+  [R.is(String), baseToPx],
+  [R.is(Array), R.map(str => stripBase(str))],
+]);
