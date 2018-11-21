44f38bb69583600f9ccad54df47b370824335d7d
maxinakenty <mgreen097@gmail.com>
2018-11-21 18:53:58 +0300

    fix separate world

--- src/helpers/separate-words/index.js
+++ src/helpers/separate-words/index.js
@@ -4,6 +4,7 @@ import { SEPARATE_STRING_INTO_WORDS_WITH_CAPITAL_LETTER } from '../../constants/
 // separateWords :: String => String -> String
 export const separateWords = (separator = '') =>
   R.compose(
+    R.toLower,
     R.join(separator),
     R.split(SEPARATE_STRING_INTO_WORDS_WITH_CAPITAL_LETTER),
   );
