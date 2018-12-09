39a12bb36f9c38ebb9f79e73e003ffd24fea8697
maxinakenty <mgreen097@gmail.com>
2018-12-09 12:43:47 +0300

    fix separate-words

--- src/helpers/separate-words/index.js
+++ src/helpers/separate-words/index.js
@@ -1,16 +1,10 @@
 import R from 'ramda';
 import { SEPARATE_STRING_INTO_WORDS_WITH_CAPITAL_LETTER } from '../../constants/regexes';
 
-// separateWords :: String => String -> String
+// separateWords :: String -> String -> String
 export const separateWords = (separator = '') =>
   R.compose(
     R.toLower,
     R.join(separator),
     R.split(SEPARATE_STRING_INTO_WORDS_WITH_CAPITAL_LETTER),
   );
-
-// toCebabCase :: String => String
-export const toCebabCase = separateWords('-');
-
-// toSnakeCase :: String => String
-export const toSnakeCase = separateWords('_');
