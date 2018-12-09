0734cf7a14969814ca1fba9b6b9c8f9e8b0786b4
maxinakenty <mgreen097@gmail.com>
2018-12-09 12:32:54 +0300

    remove api and helpers

--- api.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  ...require('./src/api/breakpoints'),
-  ...require('./src/api/make-breakpoints-model'),
-  ...require('./src/api/modular-scale'),
-  ...require('./src/api/user-config-validator'),
-};
