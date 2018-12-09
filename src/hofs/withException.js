b52649711bc023f6ef821cff023e3d8256add516
maxinakenty <mgreen097@gmail.com>
2018-12-09 12:43:30 +0300

    fix add-exception fn

--- src/hofs/withException.js
+++ src/hofs/withException.js
@@ -1,3 +1,4 @@
+// withException :: (a -> a, String) -> a -> Boolean
 export const withException = (fn, errorMessage) => x => {
   if (!(typeof fn === 'function' && fn(x))) {
     throw new Error(errorMessage);
