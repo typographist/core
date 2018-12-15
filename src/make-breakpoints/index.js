ae6b0631107b667e6dcbb216079d22ff4a5dffd0
maxinakenty <mgreen097@gmail.com>
2018-12-16 00:28:27 +0300

    add mast-memoize

--- src/make-breakpoints/index.js
+++ src/make-breakpoints/index.js
@@ -1,7 +1,10 @@
 // @flow
+import memoize from 'fast-memoize';
 import { makeBreakpointsProcess } from './make-breakpoints-process';
 import { validateUserConfig } from '../validate-user-config';
 import type { UserConfig } from '../models';
 
+const memoized = memoize(makeBreakpointsProcess);
+
 export const makeBreakpoints = (config: UserConfig) =>
-  validateUserConfig(config) ? makeBreakpointsProcess(config) : [];
+  validateUserConfig(config) ? memoized(config) : [];
