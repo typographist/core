356e674acb7205ddd9ea71758b5744fa73a32a04
maxinakenty <mgreen097@gmail.com>
2018-12-16 15:38:49 +0300

    add invariant fn with spec

---
+++ src/helpers/invariant/index.spec.js
@@ -0,0 +1,9 @@
+import { invariant } from '.';
+
+describe('helpers', () => {
+  describe('invariant fn', () => {
+    it('if the condition is not fulfilled, throw an error', () => {
+      expect(invariant(() => false, 'WTF!?')).toMatchSnapshot();
+    });
+  });
+});
