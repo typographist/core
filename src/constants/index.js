5130d07c9f4a1966b39ee7bf739d1be5f0064d44
maxinakenty <mgreen097@gmail.com>
2018-12-19 09:42:24 +0300

    remove types in constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 // @flow
 
-export const HALF: number = 0.5;
-export const BROWSER_VIEWPORT_WIDTH: string = '100vw';
-export const BROWSER_DEFAULT_FONT_SIZE: number = 16;
-export const COMMA_AND_NEW_LINE: string = ',\n';
+export const HALF = 0.5;
+export const BROWSER_VIEWPORT_WIDTH = '100vw';
+export const BROWSER_DEFAULT_FONT_SIZE = 16;
+export const COMMA_AND_NEW_LINE = ',\n';
