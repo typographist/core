f8569328f51f16b0c95571727b04b5839df3109f
maxinakenty <mgreen097@gmail.com>
2018-12-19 13:44:57 +0300

    remove old constants

--- constants.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  ...require('./src/constants/ratios'),
-  ...require('./src/constants/regexes'),
-  ...require('./src/constants/index'),
-  ratios: require('./src/constants/ratios'),
-}
