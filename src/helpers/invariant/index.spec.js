7a5741398fe3406fe2a063c27c056ad30ce3aebd
mg901 <mgreen097@gmail.com>
2018-12-19 17:38:12 +0300

    fix: update invarant fn test

--- src/helpers/invariant/index.spec.js
+++ src/helpers/invariant/index.spec.js
@@ -3,7 +3,7 @@ import { invariant } from '.';
 describe('helpers', () => {
   describe('invariant fn', () => {
     it('if the condition is not fulfilled, throw an error', () => {
-      expect(invariant(() => false, 'WTF!?')).toMatchSnapshot();
+      expect(invariant(() => false, 'WTF!?')).toEqual(undefined);
     });
   });
 });
