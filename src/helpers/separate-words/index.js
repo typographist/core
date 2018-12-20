22535a4b110a66165e828ff66e74e2c21483b4c7
mg901 <mgreen097@gmail.com>
2018-12-20 12:46:23 +0300

    fix(separate-words): change default separator

--- src/helpers/separate-words/index.js
+++ src/helpers/separate-words/index.js
@@ -3,7 +3,7 @@
 import * as constants from '../../constants';
 
 type SeparateWords = (separator?: string) => string => string;
-export const separateWords: SeparateWords = (separator = '') => str =>
+export const separateWords: SeparateWords = (separator = ' ') => str =>
   str
     .split(constants.SEPARATE_STRING_INTO_WORDS_WITH_CAPITAL_LETTER)
     .join(separator)
