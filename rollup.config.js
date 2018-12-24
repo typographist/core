37b89e673d360d933ba1d32d0e9835cb3aef08a8
mg901 <mgreen097@gmail.com>
2018-12-24 15:07:23 +0300

    feat(roolup): add rollup config

---
+++ rollup.config.js
@@ -0,0 +1,52 @@
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import { uglify } from 'rollup-plugin-uglify';
+import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+import json from 'rollup-plugin-json';
+
+import pkg from './package.json';
+
+export default [
+  // CommonJS
+  {
+    input: './src/index.js',
+    output: {
+      file: 'lib/typographist-core.cjs.js',
+      format: 'cjs',
+      indent: false,
+      sourcemap: false,
+    },
+    external: [
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {}),
+    ],
+    plugins: [babel(), uglify(), resolve(), commonjs(), json()],
+  },
+
+  // ES
+  {
+    input: './src/index.js',
+    output: {
+      file: 'es/typographist-core.es.js',
+      format: 'es',
+      indent: false,
+      sourcemap: false,
+    },
+    external: [
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {}),
+    ],
+    plugins: [
+      resolve(),
+      babel(),
+      terser(),
+      json(),
+      commonjs({
+        namedExports: {
+          'node_modules/fast-memoize/src/index.js': ['named'],
+        },
+      }),
+    ],
+  },
+];
