2066b03e5fd43a0bffde63cc65732259deaee6e2
mg901 <mgreen097@gmail.com>
2018-12-25 19:09:28 +0300

    refactor(make-breakpoints): remove fast-memoize
    replace fast-memoize with memoize-one

--- src/make-breakpoints/index.js
+++ src/make-breakpoints/index.js
@@ -1,17 +1,17 @@
 // @flow
 
-import memoize from 'fast-memoize';
+import memoizeone from 'memoize-one';
 import { makeBreakpointsProcess } from './make-breakpoints-process';
 import { validateUserConfig } from '../validate-user-config';
 import { type UserConfig } from '../models';
 
-const memoized = memoize(makeBreakpointsProcess);
+const memoizedMakeBreakpointsProcess = memoizeone(makeBreakpointsProcess);
 
 /* eslint-disable consistent-return */
 
 export const makeBreakpoints = (config: UserConfig) => {
   if (validateUserConfig(config)) {
-    return memoized(config);
+    return memoizedMakeBreakpointsProcess(config);
   }
 };
 /* eslint-enable */
