6261c612644edda1972b17b7af4c98561e78dac0
mg901 <mgreen097@gmail.com>
2018-12-29 17:07:06 +0300

    fix(semantic-release): added automatic update package.json

--- release.config.js
+++ release.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   branch: 'master',
   plugins: [
-    '@semantic-release/changelog',
     '@semantic-release/commit-analyzer',
-    '@semantic-release/git',
-    '@semantic-release/github',
-    '@semantic-release/npm',
     '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/git',
+    // '@semantic-release/github',
   ],
 };
