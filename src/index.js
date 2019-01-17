c933b8d173aebd0429349b403529d6873c7b79a5
mg901 <mgreen097@gmail.com>
2019-01-17 23:09:49 +0300

    feat(public-api): remove convertors object
    BREAKING CHANGE: Object converters is no longer available outside.
    Now each function converter is requested separately

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
+// @flow
+
 export { modularScale } from './calculators/modular-scale';
 export { makeBreakpoints } from './make-breakpoints';
 export * as constants from './constants';
-export * as convertors from './convertors';
-export * as helpers from './helpers';
+export { camelize } from './convertors/camelize';
+export { normalizeString } from './convertors/normalize-string';
+export { percentage } from './convertors/percentage';
+export { toCebabCase } from './convertors/to-cebab-case';
+export { toEmIfHasPx } from './convertors/to-em-if-has-px';
+export { toPxIfHasEm } from './convertors/to-px-if-has-em';
+export { toRem } from './convertors/to-rem';
+export { toSnakeCase } from './convertors/to-snake-case';
+export { isNumeric } from './helpers';
