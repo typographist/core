50e3d20fb77a45004feb16dcba8e5a9bc3062af7
mg901 <mgreen097@gmail.com>
2019-01-17 22:56:14 +0300

    refactor: remove convertors/index.js

--- src/convertors/index.js
+++
@@ -1,9 +0,0 @@
-export { camelize } from './camelize';
-export { percentage } from './percentage';
-export { toArrIsNotArr } from './to-arr-is-not-arr';
-export { toCebabCase } from './to-cebab-case';
-export { toEmIfHasPx } from './to-em-if-has-px';
-export { normalizeString } from './normalize-string';
-export { toPxIfHasEm } from './to-px-if-has-em';
-export { toRem } from './to-rem';
-export { toSnakeCase } from './to-snake-case';
