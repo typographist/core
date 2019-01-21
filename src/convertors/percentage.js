e4794b033f07e669713efcbc73b8571507af1bbe
mg901 <mgreen097@gmail.com>
2019-01-21 21:48:06 +0300

    fix(percentage): add the type of 'string[]' to the function parameter

--- src/convertors/percentage.js
+++ src/convertors/percentage.js
@@ -2,5 +2,5 @@
 
 import { BROWSER_DEFAULT_FONT_SIZE } from '../constants';
 
-export const percentage = (x: number | string) =>
+export const percentage = (x: number | string | number[]) =>
   `${(parseFloat(x) / BROWSER_DEFAULT_FONT_SIZE) * 100}%`;
