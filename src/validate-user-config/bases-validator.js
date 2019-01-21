07c55153a152694a24ac11ed6707c4c4afb987db
mg901 <mgreen097@gmail.com>
2019-01-21 18:38:29 +0300

    refactor(bases-validator): replace invariant with Either

--- src/validate-user-config/bases-validator.js
+++ src/validate-user-config/bases-validator.js
@@ -1,51 +1,24 @@
 // @flow
 
 import * as R from 'ramda';
-import { regexes } from '../constants';
+import { Left, Right } from 'igogo';
+import { VAL_IN_PX_OR_EM } from '../constants/regexes';
+import { BASE_ERROR_MESSAGE } from '../error-messages';
 import { getAllValuesOf } from '../helpers/get-all-values-of';
-import { invariant } from '../helpers/invariant';
-import {
-  TITLE,
-  WARNING,
-  BASE_MUST_CONTAIN_PX_OR_EM,
-  BASE_IS_STR_OR_ARR_OF_STR,
-} from '../error-messages';
+import { errorReporter } from '../helpers/error-reporter';
 import { type UserConfig } from '../models/user-config';
 
 export const getBases: (UserConfig) => * = getAllValuesOf('base');
 
-export const hasPxOrEm = R.test(regexes.PX_OR_EM_FONT_SIZE);
+export const eitherIsValidField = (field: mixed) =>
+  Array.prototype.concat.call([], field).every((f) => VAL_IN_PX_OR_EM.test(f))
+    ? Right(true)
+    : Left(BASE_ERROR_MESSAGE);
 
-export const baseLiteralIsValid = (base: string) => {
-  invariant(
-    hasPxOrEm(base),
-    `${TITLE} ${WARNING} ${BASE_MUST_CONTAIN_PX_OR_EM}`,
-  );
+export const isValidField = (field: mixed) =>
+  eitherIsValidField(field).fold(errorReporter, (x) => x);
 
-  return hasPxOrEm(base);
-};
-
-export const baseIsStrOrArrStr = (base: any) => {
-  switch (R.type(base)) {
-    case 'Array':
-      return base.every(baseLiteralIsValid);
-    case 'String':
-      return baseLiteralIsValid(base);
-    default:
-      return false;
-  }
-};
-
-export const isValidField = (base: mixed) => {
-  invariant(
-    baseIsStrOrArrStr(base),
-    `${TITLE} ${WARNING} ${BASE_IS_STR_OR_ARR_OF_STR}`,
-  );
-
-  return baseIsStrOrArrStr(base);
-};
-
-export const validateFields = (config: UserConfig) =>
-  getBases(config)
-    .map(isValidField)
-    .every(Boolean);
+export const validateFields: (UserConfig) => * = R.compose(
+  R.all(isValidField),
+  getBases,
+);
