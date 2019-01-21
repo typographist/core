cac8928fb8824cfed20988a9e846957abc820223
mg901 <mgreen097@gmail.com>
2019-01-21 18:30:58 +0300

    refactor(line-height-validator): replace invariant with Either

--- src/validate-user-config/line-heights-validator.js
+++ src/validate-user-config/line-heights-validator.js
@@ -1,33 +1,27 @@
 // @flow
 
 import * as R from 'ramda';
+import { Right, Left } from 'igogo';
+import { LINE_HEIGHT_ERRROR_MESSAGE } from '../error-messages';
 import { getAllValuesOf } from '../helpers/get-all-values-of';
+import { errorReporter } from '../helpers/error-reporter';
 import { isNumeric } from '../helpers/is-numeric';
-import { invariant } from '../helpers/invariant';
-import { TITLE, WARNING, LINE_HEIGHT_MUST_BE_NUMBER } from '../error-messages';
 import { type UserConfig } from '../models/user-config';
 
 export const getLineHeights: (UserConfig) => * = getAllValuesOf('lineHeight');
 
-export const lineHeightIsNumber = (lineHeight: mixed) => {
-  switch (R.type(lineHeight)) {
-    case 'Number':
-      return isNumeric(lineHeight);
-    default:
-      return false;
-  }
-};
+export const isValidLineHeight: (mixed) => boolean = R.both(
+  R.is(Number),
+  isNumeric,
+);
 
-export const isValidField = (lineHeight: mixed) => {
-  invariant(
-    lineHeightIsNumber(lineHeight),
-    `${TITLE} ${WARNING} ${LINE_HEIGHT_MUST_BE_NUMBER}`,
-  );
+export const eitherIsValidField = (field: mixed) =>
+  isValidLineHeight(field) ? Right(true) : Left(LINE_HEIGHT_ERRROR_MESSAGE);
 
-  return lineHeightIsNumber(lineHeight);
-};
+export const isValidField = (field: mixed) =>
+  eitherIsValidField(field).fold(errorReporter, (x) => x);
 
-export const validateFields = (config: UserConfig) =>
-  getLineHeights(config)
-    .map(isValidField)
-    .every(Boolean);
+export const validateFields: (UserConfig) => * = R.compose(
+  R.all(isValidField),
+  getLineHeights,
+);
