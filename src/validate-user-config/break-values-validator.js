8727cd2191b10a6df7c9a239bb53b8ee80afd708
mg901 <mgreen097@gmail.com>
2019-01-21 18:38:03 +0300

    refactor(break-values-validator): replace invariant with Either

--- src/validate-user-config/break-values-validator.js
+++ src/validate-user-config/break-values-validator.js
@@ -1,38 +1,29 @@
 // @flow
 
 import * as R from 'ramda';
-import { regexes } from '../constants';
+import { Right, Left } from 'igogo';
+import { VAL_IN_PX_OR_EM } from '../constants/regexes';
+import { BREAKPOINT_ERROR_MESSAGE } from '../error-messages';
 import { getAllValuesOf } from '../helpers/get-all-values-of';
-import { invariant } from '../helpers/invariant';
-import {
-  TITLE,
-  WARNING,
-  BREAK_MUST_BE_A_STRING,
-  BREAK_MUST_CONTAIN_PIXELS,
-} from '../error-messages';
+import { errorReporter } from '../helpers/error-reporter';
 import { type UserConfig } from '../models/user-config';
 
-const isString = R.is(String);
-const hasPxOrEm = R.test(regexes.PX_OR_EM_FONT_SIZE);
+export const getBreakValues: (UserConfig) => * = getAllValuesOf('breakpoint');
 
-export const getBreakValues: (UserConfig) => * = getAllValuesOf('value');
+export const hasPxOrEm: (any) => boolean = R.test(VAL_IN_PX_OR_EM);
 
-export const breakValIsStr = (val: mixed) => {
-  invariant(isString(val), `${TITLE} ${WARNING} ${BREAK_MUST_BE_A_STRING}`);
+export const isValidBreakValue: (mixed) => boolean = R.both(
+  R.is(String),
+  hasPxOrEm,
+);
 
-  return isString(val);
-};
+export const eitherIsValidField = (field: mixed) =>
+  isValidBreakValue(field) ? Right(true) : Left(BREAKPOINT_ERROR_MESSAGE);
 
-export const breakValHasPxOrEm = (val: any) => {
-  invariant(hasPxOrEm(val), `${TITLE} ${WARNING} ${BREAK_MUST_CONTAIN_PIXELS}`);
+export const isValidField = (field: mixed) =>
+  eitherIsValidField(field).fold(errorReporter, (x) => x);
 
-  return hasPxOrEm(val);
-};
-
-export const isValidField = (val: mixed) =>
-  [breakValIsStr(val), breakValHasPxOrEm(val)].every(Boolean);
-
-export const validateFields = (config: UserConfig) =>
-  getBreakValues(config)
-    .map(isValidField)
-    .every(Boolean);
+export const validateFields: (UserConfig) => * = R.compose(
+  R.all(isValidField),
+  getBreakValues,
+);
