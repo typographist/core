f995618d2b6c707c3364c91ed74a2b572788c45d
mg901 <mgreen097@gmail.com>
2019-01-21 18:36:54 +0300

    refactor: update constants

--- src/convertors/to-em-if-has-px.js
+++ src/convertors/to-em-if-has-px.js
@@ -6,6 +6,6 @@ import { BROWSER_DEFAULT_FONT_SIZE, regexes } from '../constants';
 const toEm = (x: string) => `${parseFloat(x) / BROWSER_DEFAULT_FONT_SIZE}em`;
 
 export const toEmIfHasPx: (string) => string = R.when(
-  R.test(regexes.PX_FONT_SIZE),
+  R.test(regexes.VAL_IN_PX),
   toEm,
 );
--- src/convertors/to-px-if-has-em.js
+++ src/convertors/to-px-if-has-em.js
@@ -6,6 +6,6 @@ import { BROWSER_DEFAULT_FONT_SIZE, regexes } from '../constants';
 const toPx = (x: string) => `${parseFloat(x) * BROWSER_DEFAULT_FONT_SIZE}px`;
 
 export const toPxIfHasEm: (string) => string = R.when(
-  R.test(regexes.EM_FONT_SIZE),
+  R.test(regexes.VAL_IN_EM),
   toPx,
 );
