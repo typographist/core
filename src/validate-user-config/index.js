16980d716c2c7f93c5f3a9989825cbe3c9de8f2f
mg901 <mgreen097@gmail.com>
2019-01-21 18:38:55 +0300

    refactor: add ramda allPass function

--- src/validate-user-config/index.js
+++ src/validate-user-config/index.js
@@ -1,15 +1,15 @@
 // @flow
 
+import * as R from 'ramda';
 import { validateFields as isValidBases } from './bases-validator';
 import { validateFields as isValidBreakpoints } from './break-values-validator';
 import { validateFields as isValidLineHeights } from './line-heights-validator';
 import { validateFields as isValidRatios } from './ratios-validator';
 import { type UserConfig } from '../models/user-config';
 
-export const validateUserConfig = (config: UserConfig) =>
-  [
-    isValidBases(config),
-    isValidBreakpoints(config),
-    isValidLineHeights(config),
-    isValidRatios(config),
-  ].every(Boolean);
+export const validateUserConfig: (UserConfig) => * = R.allPass([
+  isValidBases,
+  isValidBreakpoints,
+  isValidLineHeights,
+  isValidRatios,
+]);
