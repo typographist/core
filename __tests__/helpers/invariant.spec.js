7d6283d4f4c59dbf3b955c993bc20b4569b6f08a
mg901 <mgreen097@gmail.com>
2019-01-21 18:40:50 +0300

    test: remove invariant function

--- __tests__/helpers/invariant.spec.js
+++
@@ -1,7 +0,0 @@
-import { invariant } from '../../src/helpers/invariant';
-
-describe('invariant fn', () => {
-  it('if the condition is not fulfilled, throw an error', () => {
-    expect(invariant(() => false, 'WTF!?')).toEqual(undefined);
-  });
-});
