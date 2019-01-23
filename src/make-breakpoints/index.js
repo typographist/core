ba7d0c15218d5cdcfe237cf16a93314b6673efa1
mg901 <mgreen097@gmail.com>
2019-01-23 12:19:19 +0300

    fix(flow): add flow types for makeBreakpoints

--- src/make-breakpoints/index.js
+++ src/make-breakpoints/index.js
@@ -1,13 +1,14 @@
+// @flow
 import memoizeone from 'memoize-one';
 import { makeBreakpointsProcess } from './make-breakpoints-process';
 import { validateUserConfig } from '../validate-user-config';
+import { type UserConfig } from '../models/user-config';
+import { type BreakpointsMap } from '../models/breakpoints';
 
 const memoizedMakeBreakpoints = memoizeone(makeBreakpointsProcess);
 
 /* eslint-disable consistent-return */
-
-// makeBreakpoints :: Object -> {a: Object}
-export const makeBreakpoints = (config) => {
+export const makeBreakpoints = (config: UserConfig): BreakpointsMap | void => {
   if (validateUserConfig(config)) {
     return memoizedMakeBreakpoints(config);
   }
