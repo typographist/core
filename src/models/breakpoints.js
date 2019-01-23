9bb907aedfea3f561b89cfd194ff225023ef953a
mg901 <mgreen097@gmail.com>
2019-01-23 11:28:23 +0300

    chore: add breakpointsMap model

--- src/models/breakpoints.js
+++ src/models/breakpoints.js
@@ -83,3 +83,53 @@ export const breakpoints: Breakpoint[] = [
     value: '1600px',
   },
 ];
+
+export type BreakpointWithoutName = {
+  value: string,
+  base: number[],
+  lineHeight: number,
+  ratio: number,
+  root: number,
+};
+
+type BreakpointsMap = {
+  [string]: BreakpointWithoutName,
+};
+
+export const breakpointsMap: BreakpointsMap = {
+  default: {
+    base: [16, 32],
+    lineHeight: 1.5,
+    ratio: 1.1880883987824906,
+    root: 12,
+    value: '0px',
+  },
+  tablet: {
+    base: [17],
+    lineHeight: 1.5,
+    ratio: 1.1761442744249144,
+    root: 13,
+    value: '640px',
+  },
+  desktop: {
+    base: [18],
+    lineHeight: 1.7,
+    ratio: 1.333,
+    root: 16,
+    value: '1024px',
+  },
+  lgDesktop: {
+    base: [20],
+    lineHeight: 1.7,
+    ratio: 1.333,
+    root: 17,
+    value: '1200px',
+  },
+  xlDesktop: {
+    base: [22],
+    lineHeight: 1.7,
+    ratio: 1.333,
+    root: 19,
+    value: '1600px',
+  },
+};
