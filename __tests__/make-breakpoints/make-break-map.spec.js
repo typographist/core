2c4ee49af59fdb19fcfda346583b4453db7d6651
mg901 <mgreen097@gmail.com>
2019-01-23 11:41:27 +0300

    test: add tests for make-breakpoints-map function

---
+++ __tests__/make-breakpoints/make-break-map.spec.js
@@ -0,0 +1,49 @@
+import { makeBreaksMap } from '../../src/make-breakpoints/make-breaks-map';
+import { breakpoints } from '../../src/models/breakpoints';
+
+describe('makeBreaksMap', () => {
+  it('return breakpoints map', () => {
+    expect(breakpoints.reduce(makeBreaksMap, {})).toEqual({
+      default: {
+        base: [16, 32],
+        lineHeight: 1.5,
+        ratio: 1.1880883987824906,
+        root: 12,
+        value: '0px',
+        name: 'default',
+      },
+      tablet: {
+        base: [17],
+        lineHeight: 1.5,
+        ratio: 1.1761442744249144,
+        root: 13,
+        value: '640px',
+        name: 'tablet',
+      },
+      desktop: {
+        base: [18],
+        lineHeight: 1.7,
+        ratio: 1.333,
+        root: 16,
+        value: '1024px',
+        name: 'desktop',
+      },
+      lgDesktop: {
+        base: [20],
+        lineHeight: 1.7,
+        ratio: 1.333,
+        root: 17,
+        value: '1200px',
+        name: 'lgDesktop',
+      },
+      xlDesktop: {
+        base: [22],
+        lineHeight: 1.7,
+        ratio: 1.333,
+        root: 19,
+        value: '1600px',
+        name: 'xlDesktop',
+      },
+    });
+  });
+});
