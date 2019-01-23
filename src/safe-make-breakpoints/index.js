4209d1a31c402ae51e46eee0dd8773a522d34d53
mg901 <mgreen097@gmail.com>
2019-01-23 13:35:29 +0300

    refactor(make-breakpoints): rename make-breakpoints

---
+++ src/safe-make-breakpoints/index.js
@@ -0,0 +1,22 @@
+// @flow
+
+import * as R from 'ramda';
+import { Just, Nothing } from 'igogo';
+import memoizeone from 'memoize-one';
+import { makeBreakpointsProcess } from './make-breakpoints-process';
+import { validateUserConfig } from '../validate-user-config';
+import { type UserConfig } from '../models/user-config';
+import { type BreakpointsMap } from '../models/breakpoints';
+
+const memoizedMakeBreakpoints: (UserConfig) => BreakpointsMap = memoizeone(
+  makeBreakpointsProcess,
+);
+
+export const safeMakeBreakpoints: (UserConfig) => * = R.ifElse(
+  validateUserConfig,
+  R.compose(
+    Just,
+    memoizedMakeBreakpoints,
+  ),
+  Nothing,
+);
