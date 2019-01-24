9592db2a7f1599135f1eb2e259454cd9ceac40d7
mg901 <mgreen097@gmail.com>
2019-01-24 21:49:42 +0300

    refactor(to-rem): remove to-rem function

--- src/convertors/to-rem.js
+++
@@ -1,6 +0,0 @@
-// @flow
-
-import { type Breakpoint } from '../models/breakpoints';
-
-export const toRem = (fontSize: number | number[], { root }: Breakpoint) =>
-  `${parseFloat(fontSize) / root}rem`;
