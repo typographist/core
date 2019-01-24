0771571f7c0f570957ba0f23f49f60960ce01640
mg901 <mgreen097@gmail.com>
2019-01-24 22:01:34 +0300

    test: fix tests

--- __tests__/calculators.spec.js
+++ __tests__/calculators.spec.js
@@ -14,11 +14,11 @@ describe('calculators', () => {
 
   describe('calcFontSize', () => {
     it('calculated `font-size` if base is array of numbers', () => {
-      expect(modularScale(7)({ base: [16, 33, 15], ratio: 1.5 })).toEqual(49.5);
+      expect(modularScale(7, { base: [16, 33, 15], ratio: 1.5 })).toEqual(49.5);
     });
 
     it('calculated `font-size` if base is array of numbers', () => {
-      expect(modularScale(6)({ base: [16], ratio: 1.333 })).toEqual(
+      expect(modularScale(6, { base: [16], ratio: 1.333 })).toEqual(
         89.76372759879813,
       );
     });
