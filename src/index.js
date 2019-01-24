b294ded9718d6c3d22769ad24fb836b1819e31c9
mg901 <mgreen097@gmail.com>
2019-01-24 22:03:26 +0300

    refactor: remove helpers and modular-scale
    BREAKING CHANGE: remove fuctions modular-scale, camelize,
    normalizeString, toCebabCase, toPxIfHasEm, toSnakeCase, isNumeric

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 // @flow
 
-export { modularScale } from './calculators/modular-scale';
+// export { modularScale } from './calculators/modular-scale';
 export { makeBreakpoints } from './make-breakpoints';
 export * as constants from './constants';
-export { camelize } from './convertors/camelize';
-export { normalizeString } from './convertors/normalize-string';
+// export { camelize } from './convertors/camelize';
+// export { normalizeString } from './convertors/normalize-string';
 export { percentage } from './convertors/percentage';
-export { toCebabCase } from './convertors/to-cebab-case';
+// export { toCebabCase } from './convertors/to-cebab-case';
 export { toEmIfHasPx } from './convertors/to-em-if-has-px';
-export { toPxIfHasEm } from './convertors/to-px-if-has-em';
-export { toRem } from './convertors/to-rem';
-export { toSnakeCase } from './convertors/to-snake-case';
-export { isNumeric } from './helpers/is-numeric';
+// export { toPxIfHasEm } from './convertors/to-px-if-has-em';
+// export { toSnakeCase } from './convertors/to-snake-case';
+// export { isNumeric } from './helpers/is-numeric';
