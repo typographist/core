004d0c9b69b6782c063e89a530a31c9d22d88800
mg901 <mgreen097@gmail.com>
2019-01-27 00:18:27 +0300

    test: update tests for modular-scale

--- __tests__/calculators.spec.js
+++ __tests__/calculators.spec.js
@@ -12,13 +12,13 @@ describe('calculators', () => {
     });
   });
 
-  describe('calcFontSize', () => {
+  describe('modularScale', () => {
     it('calculated `font-size` if base is array of numbers', () => {
-      expect(modularScale(7, { base: [16, 33, 15], ratio: 1.5 })).toEqual(49.5);
+      expect(modularScale({ base: [16, 33, 15], ratio: 1.5 })(7)).toEqual(49.5);
     });
 
     it('calculated `font-size` if base is array of numbers', () => {
-      expect(modularScale(6, { base: [16], ratio: 1.333 })).toEqual(
+      expect(modularScale({ base: [16], ratio: 1.333 })(6)).toEqual(
         89.76372759879813,
       );
     });
