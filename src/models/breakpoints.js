2e3485bdb95d6ee9af710a149da9b64dff7f5f0c
mg901 <mgreen097@gmail.com>
2019-02-04 00:21:53 +0300

    chore: update models

--- src/models/breakpoints.js
+++ src/models/breakpoints.js
@@ -1,135 +1,90 @@
 // @flow
 
-export type NotFilledBreakpoint = {
-  breakpoint: string,
+export type UnstableDefaultBreak = {|
+  base: string | string[],
+  lineHeight: number,
+  ratio: number | string,
   name: string,
-  base?: number[],
-  lineHeight?: number,
-  ratio?: number | string,
-};
+  value: string,
+|};
 
-export type NamedBreakpoint = {
+export type NotFilledDefaultBreakWithoutName = {|
   breakpoint: string,
-  name: string,
   base?: string | string[],
   lineHeight?: number,
   ratio?: number | string,
-};
+|};
 
-export type FilledBreakpoint = {|
+export type NotFilledNamedBreak = {|
+  base?: string | string[],
+  lineHeight?: number,
+  ratio?: number | string,
+  name: string,
+  breakpoint: string,
+|};
+
+export type NotFilledRenamedBreak = {|
+  base?: string | string[],
+  lineHeight?: number,
+  ratio?: number | string,
+  name: string,
   value: string,
+|};
+
+export type FillledBreak = {|
+  base: string | string[],
+  lineHeight: number,
+  ratio: number | string,
+  name: string,
+  value: string,
+|};
+
+export type BreakStableBase = {|
   base: number[],
   lineHeight: number,
   ratio: number | string,
   name: string,
+  value: string,
 |};
 
-export type FilledBreakpointStableRatio = {|
-  value: string,
+export type BreakStableRatio = {|
   base: number[],
   lineHeight: number,
   ratio: number,
   name: string,
+  value: string,
 |};
 
-export type Breakpoint = {
+export type BreakWithRoot = {|
+  base: number[],
+  lineHeight: number,
+  ratio: number,
+  name: string,
+  value: string,
+  root: number,
+|};
+
+export type BreakpointWithName = {|
   value: string,
   base: number[],
   lineHeight: number,
   ratio: number,
   root: number,
   name: string,
+|};
+
+export type UnstableBreaksMap = {
+  [string]: BreakpointWithName,
 };
 
-export const breakpoints: Breakpoint[] = [
-  {
-    base: [16, 32],
-    lineHeight: 1.5,
-    name: 'default',
-    ratio: 1.1880883987824906,
-    root: 12,
-    value: '0px',
-  },
-  {
-    base: [17],
-    lineHeight: 1.5,
-    name: 'tablet',
-    ratio: 1.1761442744249144,
-    root: 13,
-    value: '640px',
-  },
-  {
-    base: [18],
-    lineHeight: 1.7,
-    name: 'desktop',
-    ratio: 1.333,
-    root: 16,
-    value: '1024px',
-  },
-  {
-    base: [20],
-    lineHeight: 1.7,
-    name: 'lgDesktop',
-    ratio: 1.333,
-    root: 17,
-    value: '1200px',
-  },
-  {
-    base: [22],
-    lineHeight: 1.7,
-    name: 'xlDesktop',
-    ratio: 1.333,
-    root: 19,
-    value: '1600px',
-  },
-];
-
-export type BreakpointWithoutName = {
+export type Breakpoint = {|
   value: string,
   base: number[],
   lineHeight: number,
   ratio: number,
   root: number,
-};
+|};
 
 export type BreakpointsMap = {
-  [string]: BreakpointWithoutName,
-};
-
-export const breakpointsMap: BreakpointsMap = {
-  default: {
-    base: [16, 32],
-    lineHeight: 1.5,
-    ratio: 1.1880883987824906,
-    root: 12,
-    value: '0px',
-  },
-  tablet: {
-    base: [17],
-    lineHeight: 1.5,
-    ratio: 1.1761442744249144,
-    root: 13,
-    value: '640px',
-  },
-  desktop: {
-    base: [18],
-    lineHeight: 1.7,
-    ratio: 1.333,
-    root: 16,
-    value: '1024px',
-  },
-  lgDesktop: {
-    base: [20],
-    lineHeight: 1.7,
-    ratio: 1.333,
-    root: 17,
-    value: '1200px',
-  },
-  xlDesktop: {
-    base: [22],
-    lineHeight: 1.7,
-    ratio: 1.333,
-    root: 19,
-    value: '1600px',
-  },
+  [string]: Breakpoint,
 };
