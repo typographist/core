bf0e7716f15b1e59bd74ac617bbcfdb165d0f3fe
mg901 <mgreen097@gmail.com>
2019-02-04 00:15:28 +0300

    style: add linters field

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   'lint-staged': {
-    '*.js': ['eslint --fix', 'prettier --write', 'git add'],
-    '*.{json,js,eslintrc}': ['prettier --write', 'git add'],
+    linters: {
+      '*.{js,json}': ['eslint --fix', 'prettier --write', 'git add'],
+    },
   },
 };
