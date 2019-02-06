2778f8ddce07bb5ad007e097d01aac87a81740ef
mg901 <mgreen097@gmail.com>
2019-02-06 18:31:42 +0300

    refactor(ratios): remove export ratios from constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,7 @@
 // @flow
 
 export * as regexes from './regexes';
-export * as ratios from './ratios';
-export const COMMA_AND_NEW_LINE = ',\n'; // postcss
 export const HALF = 0.5;
 export const BROWSER_VIEWPORT_WIDTH = '100vw';
 export const BROWSER_DEFAULT_FONT_SIZE = 16;
+export const COMMA_AND_NEW_LINE = ',\n'; // postcss
