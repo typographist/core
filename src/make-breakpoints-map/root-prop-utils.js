774299bc5f5581f7960545141f15196ab5a59de3
mg901 <mgreen097@gmail.com>
2019-02-06 16:42:24 +0300

    feat(calc-root): removed the rounding to a whole number

--- src/make-breakpoints-map/root-prop-utils.js
+++ src/make-breakpoints-map/root-prop-utils.js
@@ -5,7 +5,7 @@ import { HALF } from '../constants';
 import { calcLeading } from '../calculators/calc-leading';
 import type { BreakStableRatio, BreakWithRoot } from '../models';
 
-export const calcRoot = (x: number) => Math.round(x * HALF);
+export const calcRoot = (leading: number) => leading * HALF;
 
 export const setPropRoot: (BreakStableRatio) => BreakWithRoot = (item) => ({
   ...item,
