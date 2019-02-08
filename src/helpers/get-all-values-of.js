983da8a985b49a55389f18773594bd765c53f61d
mg901 <mgreen097@gmail.com>
2019-02-08 22:09:06 +0300

    feat: rewritten in an immutable style

--- src/helpers/get-all-values-of.js
+++ src/helpers/get-all-values-of.js
@@ -1,20 +1,16 @@
 // @flow
 
-import { isObject } from './is-object';
-
 export const getAllValuesOf: (string, void | any[]) => (any) => any[] = (
   target,
   memo,
 ) => (obj) => {
   const buffer = !Array.isArray(memo) ? [] : memo;
 
-  for (const key in obj) {
-    if (key === target) {
-      buffer.push(obj[key]);
-    } else if (Array.isArray(obj[key]) || isObject(obj[key])) {
-      getAllValuesOf(target, buffer)(obj[key]);
-    }
-  }
-
-  return buffer;
+  return Object.keys(Object(obj)).reduce(
+    (acc, key) =>
+      key === target
+        ? [...acc, obj[key]]
+        : [...getAllValuesOf(target, acc)(obj[key])],
+    buffer,
+  );
 };
