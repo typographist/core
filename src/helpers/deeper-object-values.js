9704e99e9bb8481b25b6af66b30ed908eb470dc4
mg901 <mgreen097@gmail.com>
2019-02-09 11:35:16 +0300

    refactor(deep-object-values): remove buffer

--- src/helpers/deeper-object-values.js
+++ src/helpers/deeper-object-values.js
@@ -3,17 +3,17 @@
 export const deeperObjectValues: (string, void | any[]) => (any) => any[] = (
   target,
   memo,
-) => (obj) => {
-  const buffer = !Array.isArray(memo) ? [] : memo;
+) => (obj) =>
+  Object.keys(Object(obj)).reduce(
+    (acc, key) => {
+      if (key === target) {
+        return [...acc, obj[key]];
+      }
+      if (obj[key] instanceof Object) {
+        return [...deeperObjectValues(target, acc)(obj[key])];
+      }
 
-  return Object.keys(Object(obj)).reduce((acc, key) => {
-    if (key === target) {
-      return [...acc, obj[key]];
-    }
-    if (obj[key] instanceof Object) {
-      return [...deeperObjectValues(target, acc)(obj[key])];
-    }
-
-    return acc;
-  }, buffer);
-};
+      return acc;
+    },
+    !Array.isArray(memo) ? [] : memo,
+  );
