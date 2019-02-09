ebc998a38f3a3bdf2d82b11ecc08a76211544814
mg901 <mgreen097@gmail.com>
2019-02-09 11:22:57 +0300

    fix(get-all-values-of):  remove maximum call stack error

--- src/helpers/get-all-values-of.js
+++ src/helpers/get-all-values-of.js
@@ -6,11 +6,14 @@ export const getAllValuesOf: (string, void | any[]) => (any) => any[] = (
 ) => (obj) => {
   const buffer = !Array.isArray(memo) ? [] : memo;
 
-  return Object.keys(Object(obj)).reduce(
-    (acc, key) =>
-      key === target
-        ? [...acc, obj[key]]
-        : [...getAllValuesOf(target, acc)(obj[key])],
-    buffer,
-  );
+  return Object.keys(Object(obj)).reduce((acc, key) => {
+    if (key === target) {
+      return [...acc, obj[key]];
+    }
+    if (obj[key] instanceof Object) {
+      return [...getAllValuesOf(target, acc)(obj[key])];
+    }
+
+    return acc;
+  }, buffer);
 };
