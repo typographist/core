69fb656d33c5110b5b4c6107ea8b8fdc0beba9ce
mg901 <mgreen097@gmail.com>
2019-02-28 06:54:34 +0300

    build(rollup): refactor config

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,32 @@
+import * as R from 'ramda';
 import resolve from 'rollup-plugin-node-resolve';
 import { uglify } from 'rollup-plugin-uglify';
 import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 import flowEntry from 'rollup-plugin-flow-entry';
-import pkg from './package.json';
+import { main, module, dependencies, peerDependencies } from './package.json';
+
+const ENV = process.env.NODE_ENV;
+const ES = ENV === 'es';
+const CJS = ENV === 'cjs';
+const isCoreModules = [ES, CJS].some(Boolean);
+
+const outputFile = isCoreModules && CJS ? main : module;
+const plugins = [babel(), resolve(), flowEntry()];
 
 export default [
   {
     input: './src/index.js',
     output: {
-      file: 'lib/index.cjs.js',
-      format: 'cjs',
+      file: outputFile,
+      format: ENV,
       indent: false,
       sourcemap: true,
     },
     external: [
-      ...Object.keys(pkg.dependencies || {}),
-      ...Object.keys(pkg.peerDependencies || {}),
+      ...Object.keys(dependencies || {}),
+      ...Object.keys(peerDependencies || {}),
     ],
-    plugins: [babel(), resolve(), uglify(), flowEntry()],
-  },
-  {
-    input: './src/index.js',
-    output: {
-      file: 'es/index.es.js',
-      format: 'es',
-      indent: false,
-      sourcemap: true,
-    },
-    external: [
-      ...Object.keys(pkg.dependencies || {}),
-      ...Object.keys(pkg.peerDependencies || {}),
-    ],
-    plugins: [babel(), resolve(), terser(), flowEntry()],
+    plugins: CJS ? [...plugins, uglify()] : [...plugins, terser()],
   },
 ];
