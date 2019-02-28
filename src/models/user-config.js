9bba9e95610ee66ac1c4e0fe4398d58652ff13b3
mg901 <mgreen097@gmail.com>
2019-02-28 06:57:19 +0300

    refactor(models): remove user-config

--- src/models/user-config.js
+++ src/models/user-config.js
@@ -11,27 +11,3 @@ export type UserConfig = {
     ratio?: number,
   },
 };
-
-export const userConfig: UserConfig = {
-  base: ['1em', '2em'],
-  lineHeight: 1.5,
-  ratio: '45px at 6',
-  tablet: {
-    breakpoint: '40em',
-    base: '17px',
-  },
-  desktop: {
-    breakpoint: '64em',
-    base: '18px',
-    lineHeight: 1.7,
-    ratio: 1.333,
-  },
-  lgDesktop: {
-    breakpoint: '75em',
-    base: '20px',
-  },
-  xlDesktop: {
-    breakpoint: '100em',
-    base: '22px',
-  },
-};
