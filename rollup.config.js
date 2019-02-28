43517fa0d22a9d7434d0ca668e1d7c8fb4bc67fc
mg901 <mgreen097@gmail.com>
2019-02-28 08:04:02 +0300

    build(rollup): add entry for public utils

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,3 @@
-import * as R from 'ramda';
 import resolve from 'rollup-plugin-node-resolve';
 import { uglify } from 'rollup-plugin-uglify';
 import babel from 'rollup-plugin-babel';
@@ -9,16 +8,32 @@ import { main, module, dependencies, peerDependencies } from './package.json';
 const ENV = process.env.NODE_ENV;
 const ES = ENV === 'es';
 const CJS = ENV === 'cjs';
+
+const plugins = [babel(), resolve(), flowEntry()];
 const isCoreModules = [ES, CJS].some(Boolean);
 
-const outputFile = isCoreModules && CJS ? main : module;
-const plugins = [babel(), resolve(), flowEntry()];
+const indexOutput = isCoreModules && CJS ? main : module;
+const utilsOutput = CJS ? 'dist/lib/utils.cjs.js' : 'dist/es/utils.es.js';
 
 export default [
   {
     input: './src/index.js',
     output: {
-      file: outputFile,
+      file: indexOutput,
+      format: ENV,
+      indent: false,
+      sourcemap: true,
+    },
+    external: [
+      ...Object.keys(dependencies || {}),
+      ...Object.keys(peerDependencies || {}),
+    ],
+    plugins: CJS ? [...plugins, uglify()] : [...plugins, terser()],
+  },
+  {
+    input: './src/utils-public/index.js',
+    output: {
+      file: utilsOutput,
       format: ENV,
       indent: false,
       sourcemap: true,
