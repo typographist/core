029ebb4fc1d04662aaea2a844522bf5a0c5f6c89
mg901 <mgreen097@gmail.com>
2019-03-03 01:05:40 +0300

    chore(utils-public): add index.js

---
+++ src/utils-public/index.js
@@ -0,0 +1,8 @@
+// @flow
+export { camelize } from './camelize';
+export { modularScale } from './modular-scale';
+export { normalizeString } from './normalize-string';
+export { separateWords } from './separate-words';
+export { toCebabCase } from './to-cebab-case';
+export { toRem } from './to-rem';
+export { toSnakeCase } from './to-snake-case';
