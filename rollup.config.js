413c19c6deaf213daaae68dc8011e32bfab712e0
mg901 <mgreen097@gmail.com>
2019-03-25 00:51:51 +0300

    build(rollup): remove flow-entry plugin

--- rollup.config.js
+++ rollup.config.js
@@ -2,14 +2,13 @@ import resolve from 'rollup-plugin-node-resolve';
 import { uglify } from 'rollup-plugin-uglify';
 import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
-import flowEntry from 'rollup-plugin-flow-entry';
 import { main, module, dependencies, peerDependencies } from './package.json';
 
 const ENV = process.env.NODE_ENV;
 const ES = ENV === 'es';
 const CJS = ENV === 'cjs';
 
-const plugins = [babel(), resolve(), flowEntry()];
+const plugins = [babel(), resolve()];
 const isCoreModules = [ES, CJS].some(Boolean);
 const indexOutput = isCoreModules && CJS ? main : module;
 
