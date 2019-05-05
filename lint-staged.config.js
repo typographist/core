b41dec6e8b1bea8fedf74a37f4a9a94cab831d29
mg901 <mgreen097@gmail.com>
2019-05-06 01:32:05 +0300

    fix: fix lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  'lint-staged': {
-    linters: {
-      '*.{js,json}': ['eslint --fix', 'prettier --write', 'git add'],
-    },
+  linters: {
+    '*.{js,json}': ['eslint --fix', 'prettier --write', 'git add'],
   },
 };
