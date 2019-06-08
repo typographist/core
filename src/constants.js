9997c0a3916cfaacb6671b1662f58c8dac78a64c
mg901 <mgreen097@gmail.com>
2019-06-09 01:37:33 +0300

    refactor: remove global constants

--- src/constants.js
+++
@@ -1,7 +0,0 @@
-export const HALF = 0.5;
-export const BROWSER_CONTEXT = 16;
-export const POSITIVE_OR_NEGATIVE_INT_OR_FLOAT_NUM_AT_BEGINNING_OF_THE_STRING = /^-?\d+(\.\d+)?$/;
-export const DASH_HYPHEN_WHITESPACE_ANY_CHARACTERS = /[-_\s]+(.)?/g;
-export const SEPARATE_STRING_INTO_WORDS_WITH_CAPITAL_LETTER = /(?=[A-Z])/;
-export const FONT_SIZE = /\b\d+(\.\d+)?(px|em)\b/g;
-export const POSITIVE_OR_NEGATIVE_INT_OR_FLOAT_NUM_AT_END_OF_STRING = /-?\b\d+(\.\d+)?\b\s*$/g;
