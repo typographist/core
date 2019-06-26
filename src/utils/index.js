3683436224114ddc9a6c5b2c6cb44b65d9b911c0
mg901 <mgreen097@gmail.com>
2019-06-26 13:00:16 +0300

    refactor: remove functions getFirstLetter, getWordTail, camelize, normalizeString, separateWords, toKebabCase, toSnakeCase
    BREAKING CHANGE: getFirstLetter, getWordTail, camelize, normalizeString, separateWords,
    toKebabCase, toSnakeCase

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,34 +6,3 @@ export const SEPARATE_STRING_INTO_WORDS_WITH_CAPITAL_LETTER = /(?=[A-Z])/;
 export const invariant = invariantWithPrefix(
   '[typographist]: Check your config.',
 );
-
-// getFirstLetter :: String -> String
-export const getFirstLetter = (word) => word[0].toLowerCase();
-
-// getWordTail :: String -> String
-export const getWordTail = (word) =>
-  word
-    .slice(1)
-    .replace(DASH_HYPHEN_WHITESPACE_ANY_CHARACTERS, (_, chr) =>
-      chr.toUpperCase(),
-    );
-
-// camelize :: String -> String
-export const camelize = (str) => `${getFirstLetter(str)}${getWordTail(str)}`;
-
-// normalizeString :: String -> String
-export const normalizeString = (x) =>
-  x.replace(/([a-z])([A-Z])/g, '$1 $2').toLowerCase();
-
-// separateWords :: (Maybe String) -> String -> String
-export const separateWords = (separator = ' ') => (str) =>
-  str
-    .split(SEPARATE_STRING_INTO_WORDS_WITH_CAPITAL_LETTER)
-    .join(separator)
-    .toLowerCase();
-
-// toKebabCase :: String -> String
-export const toKebabCase = separateWords('-');
-
-// toSnakeCase :: String -> String
-export const toSnakeCase = separateWords('_');
